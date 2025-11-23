37900298f6876d5e24a52a28ce7b1c83f3379913
AsmiiGaurav <agaurav_be22@thapar.edu>
2025-11-23 23:57:09 +0530

    Add debug overlay

--- src/ARScene.jsx
+++ src/ARScene.jsx
@@ -1,5 +1,22 @@
 export default function ARScene() {
+  useEffect(() => {
+  const log = (msg) => {
+    const el = document.getElementById("debug-log");
+    el.innerText += msg + "\n";
+  };
+
+  // Camera error capture
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(() => log("Camera OK"))
+    .catch((err) => log("Camera Error: " + err.name + " - " + err.message));
+
+  window.onerror = function (msg, url, line) {
+    log("JS Error: " + msg + " @ " + line);
+  };
+}, []);
+
   return (
+  <>
     <a-scene
       mindar-image="imageTargetSrc: /targets.mind"
       color-space="sRGB"
@@ -25,5 +42,24 @@ export default function ARScene() {
         ></a-box>
       </a-entity>
     </a-scene>
-  );
+
+    {/* Debug log box */}
+    <div
+      id="debug-log"
+      style={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        width: "100%",
+        maxHeight: "180px",
+        background: "rgba(0, 0, 0, 0.85)",
+        color: "#00ff00",
+        fontSize: "12px",
+        padding: "10px",
+        overflowY: "scroll",
+        zIndex: 9999
+      }}
+    ></div>
+  </>
+);
 }
